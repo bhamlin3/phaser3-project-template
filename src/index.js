20bcd7cd555c84cca220160cbea497bbfebe60ef
Brian Hamlin <bhamlin92@gmail.com>
2018-07-07 16:12:06 -0400

    Add editorconfig

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import 'phaser';
 import GameScene from './scenes/GameScene';
 
 const config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: [
-        GameScene
-    ]
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 800,
+  height: 600,
+  scene: [
+    GameScene
+  ]
 };
 
 const game = new Phaser.Game(config);
